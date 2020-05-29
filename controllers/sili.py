08e33bd04755f46a3a924488a30206c6f006c641
tribo.ni.eila <tribo.ni.eila@gmail.com>
2020-05-29 23:05:17 +0300

    updated files

--- controllers/sili.py
+++ controllers/sili.py
@@ -1,20 +1,30 @@
 def labuyo():
-    form = SQLFORM(db.auth_user)
-    if form.process().accepted:
-        response.flash = 'RECORD SAVE'
-    elif form.errors:
-        response.flash = 'FORM HAS ERRORS'
     row = []
     head = THEAD(TR(TH('#'),TH('First Name'),TH('Last Name'),TH('Role'),TH('Email'),TH('Action',_class='sorting_disabled')))
     for u in db().select(db.auth_user.ALL, db.auth_membership.ALL, db.auth_group.ALL, orderby = db.auth_user.id, 
     left = [db.auth_membership.on(db.auth_membership.user_id == db.auth_user.id),db.auth_group.on(db.auth_group.id == db.auth_membership.group_id)]):
-        edit_lnk = A(I(_class='fas fa-pencil-alt'), _title='Edit Row', _type='button  ', _role='button', _class='btn btn-icon-toggle', _href=URL('labuyo_edit_form', args = u.auth_user.id))
-        dele_lnk = A(I(_class='fas fa-trash-alt'), _title='Delete Row', _type='button  ', _role='button', _class='btn btn-icon-toggle', _href=URL('#', args = u.auth_user.id))
-        btn_lnks = DIV(edit_lnk, dele_lnk, _class="hidden-sm hidden-xs action-buttons")
+        view_lnk = A(I(_class='fa fa-search'), _title='View Row', _type='button  ', _role='button', _class='btn btn-icon-toggle disabled', _href=URL('#', args = u.auth_user.id))        
+        edit_lnk = A(I(_class='fa fa-pencil'), _title='Edit Row', _type='button  ', _role='button', _class='btn btn-icon-toggle', _href = (URL('sili','post_user_form', args = u.auth_user.id))) #callback = URL(args = n.status), **{'_data-id':(n.id),'_data-sta':(n.status), '_data-des':(n.description)})
+        dele_lnk = A(I(_class='fa fa-trash'), _title='Delete Row', _type='button  ', _role='button', _class='btn btn-icon-toggle disabled', _href=URL('#', args = u.auth_user.id))        
+        btn_lnks = DIV(view_lnk,edit_lnk, dele_lnk)
         row.append(TR(TD(u.auth_user.id),TD(u.auth_user.first_name.upper()),TD(u.auth_user.last_name.upper()),TD(u.auth_group.role),TD(u.auth_user.email.lower()),TD(btn_lnks)))
     body = TBODY(*row)
-    table = TABLE(*[head, body], _class='table table-striped')    
-    return dict(form = form, table = table)
+    table = TABLE(*[head, body], _class='table')     
+    return dict(table = table)
+
+def post_user_form():
+    _id = db(db.auth_user.id == request.args(0)).select().first()
+    if _id:
+        db.auth_user.id.readable = False
+        form = SQLFORM(db.auth_user, _id)
+    else:
+        form = SQLFORM(db.auth_user)
+    if form.process().accepted:
+        response.flash = 'RECORD SAVE'
+        redirect(URL('sili','labuyo'))
+    elif form.errors:
+        response.flash = 'FORM HAS ERRORS'
+    return dict(form = form)
 
 def labuyo_edit_form():
     form =SQLFORM(db.auth_user, request.args(0))
